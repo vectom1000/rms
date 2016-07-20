5a3b8a3147925a0896f604a9eac67e9c03adca92
julian.fink@web.de <jumbomira1000>
2016-07-21 00:52:24 +0200

    Funktion mit Docker

--- install.sh
+++ install.sh
@@ -1,24 +1,14 @@
-# Create common network
-docker network create simple-network
-
 # Create and start Exposee-Container, and create link with other containers
 docker build -t rmsimage .
 echo -n "Building RMS"
-while true;do echo -n .;sleep 1;done &
-sleep 1 # or do something else here
-kill $!; trap 'kill $!' SIGTERM
 echo 'done'
-docker run --name rmsapp --net=simple-network  -p 8000:8000 -d rmsimage
+docker run --name rmsapp -p 8000:8000 -d rmsimage
 echo -n "makemigrations... "
 docker exec -d rmsapp python manage.py makemigrations
 echo "done"
 echo -n "migrate"
-docker exec -d rmsapp python manage.py migrate
-while true;do echo -n .;sleep 1;done &
-sleep 1 # or do something else here
-kill $!; trap 'kill $!' SIGTERM
 echo 'done'
-echo -n "init_db... "
+#echo -n "init_db... "
 #docker exec -i rmsapp python manage.py shell < init_db.py
 #python manage.py migrate --run-syncdb
 echo "RMS is now ready!"
