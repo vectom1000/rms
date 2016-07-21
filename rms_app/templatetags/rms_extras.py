e2f582cc2242de04b8772113ebad12fd919706d4
julian.fink@web.de <jumbomira1000>
2016-07-21 13:21:34 +0200

    - EIgene Filter - Dynamische Ausgabe des Bildes

--- rms_app/templatetags/rms_extras.py
+++ rms_app/templatetags/rms_extras.py
@@ -1,9 +1,11 @@
 from django import template
+from django.conf.urls.static import static
 
 register = template.Library()
 
 
 @register.filter
-def gib_photo_string(value, arg):
+def gib_photo_string(value):
     """Removes all values of arg from the given string"""
-    return value
+    path = '/static/rms_app/photos/{0}.jpg'.format(value)
+    return path
