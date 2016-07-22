d3df03c2dc7dc4ebca3d6738ac853043c0ec4d42
julian.fink@web.de <jumbomira1000>
2016-07-22 18:28:09 +0200

    - css angepasst

--- rms_app/views.py
+++ rms_app/views.py
@@ -6,10 +6,8 @@
 # Django Module/Klassen
 from django.http import HttpResponseNotFound
 from django.shortcuts import render, HttpResponseRedirect
-from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
-from django.contrib.auth import login
 from django.core.urlresolvers import reverse
 from rms_app.models import Gericht
 from django.contrib.auth import authenticate, login
