2d11686967729a029630988899759c43a8d997c2
julian.fink@web.de <jumbomira1000>
2016-07-23 01:54:01 +0200

    - Mit Formular begonnen

--- rms_app/forms.py
+++ rms_app/forms.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
-"""Modul for Forms of Thesis-Exposee."""
+"""Modul for Forms of RMS."""
+from django import forms
+from django.forms import ModelForm
+from bootstrap_markdown.widgets import MarkdownEditor
+from rms_app.models import Kategorie, Gericht
+
+
+class GerichtsFormular(ModelForm):
+    gericht_name = forms.CharField(max_length=1500, required=True, widget=MarkdownEditor(
+                                            attrs={'id': 'Name',
+                                                   'boostrap_cdn': False,
+                                                   'hiddenButtons': "Preview"}))
 
 
