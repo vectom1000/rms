8a2e50fe0fe052348298c41f4a1d6fd017b39e01
julian.fink@web.de <jumbomira1000>
2016-07-23 00:41:27 +0200

    - Admin Interface angepasst

--- rms_app/admin.py
+++ rms_app/admin.py
@@ -1,9 +1,10 @@
 """Modul for Admin of Thesis-Exposee."""
 from django.contrib import admin
-from rms_app.models import Gericht
+from rms_app.models import Gericht, Kategorie
 
 
 class GerichtAdmin(admin.ModelAdmin):
         list_display = ('gericht_name', 'beschreibung')
 
 admin.site.register(Gericht, GerichtAdmin)
+admin.site.register(Kategorie)
