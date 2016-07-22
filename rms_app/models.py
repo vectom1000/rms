e1527988b0322f55bf5d10625a8e9551d3d0c87a
julian.fink@web.de <jumbomira1000>
2016-07-23 01:47:11 +0200

    - Modells angepasst

--- rms_app/models.py
+++ rms_app/models.py
@@ -6,9 +6,10 @@ from django.contrib.auth.models import User
 
 
 class Gericht(models.Model):
-    gericht_name = models.CharField(max_length=100)
-    beschreibung = models.CharField(max_length=20000)
-    kategorie = models.ForeignKey('Kategorie', on_delete=models.PROTECT, null=True, related_name='+')
+    gericht_name = models.CharField(max_length=100, blank=False)
+    beschreibung = models.CharField(max_length=20000, blank=True)
+    kategorie = models.ForeignKey('Kategorie', on_delete=models.PROTECT,
+                                  null=True, related_name='+', blank=True)
 
     def __str__(self):
         return self.gericht_name
@@ -19,7 +20,7 @@ class Gericht(models.Model):
 
 
 class Kategorie(models.Model):
-    kategorie_name = models.CharField(max_length=100)
+    kategorie_name = models.CharField(max_length=100, blank=False)
 
     def __str__(self):
         return self.kategorie_name
