83c794e154ff360642d2fbb1e4a4cf7a89dc27e1
julian.fink@web.de <jumbomira1000>
2016-07-22 16:29:13 +0200

    - Skript angepasst - Umstieg auf Python 3.5

--- install.sh
+++ install.sh
@@ -7,11 +7,13 @@ echo -n "makemigrations... "
 docker exec -d rmsapp python manage.py makemigrations
 echo "done"
 echo -n "migrate"
+docker exec -i rmsapp python manage.py migrate
 echo 'done'
 #echo -n "init_db... "
-#docker exec -i rmsapp python manage.py shell < init_db.py
+sleep 1
+docker exec -i rmsapp python manage.py shell < init_db2.py
 #python manage.py migrate --run-syncdb
 #python manage.py makemigrations rms_app
 echo "RMS is now ready!"
-
+#python manage.py migrate --run-syncdb
 
--- rms_app/admin.py
+++ rms_app/admin.py
@@ -1,13 +1,9 @@
 """Modul for Admin of Thesis-Exposee."""
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
-from django.db.models import Q
 from rms_app.models import Gericht
 
 
 class GerichtAdmin(admin.ModelAdmin):
         list_display = ('gericht_name', 'beschreibung')
-
 
 admin.site.register(Gericht, GerichtAdmin)
