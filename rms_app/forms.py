4ba0d88a05afa996615394565e8f36633b146bd2
julian.fink@web.de <jumbomira1000>
2016-07-24 13:42:02 +0200

    -Bild hinzugefügt

--- rms_app/forms.py
+++ rms_app/forms.py
@@ -10,9 +10,7 @@ class GerichtsFormular(ModelForm):
     gericht_name = forms.CharField(max_length=300, required=True, widget=forms.TextInput(
         attrs={"type": "text", "class": "form-control", 'autofocus': 'autofocus'}))
     beschreibung = forms.CharField(max_length=3000, required=True, widget=MarkdownEditor(
-                                            attrs={'id': 'Beschreibung',
-                                                   'hiddenButtons': "Preview",
-                                                   'fullscreen': True}))
+        attrs={'id': 'Beschreibung', 'hiddenButtons': "Preview", 'fullscreen': True}))
     kategorie = forms.ModelChoiceField(queryset=Kategorie.objects.all(), widget=forms.Select(
         attrs={"class": "form-control", }))
 
